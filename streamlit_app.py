687c0ceb78ef21055f12b49395ef51ede0f29487
MedusaNaut <144046999+MedusaNaut@users.noreply.github.com>
2026-09-11 14:13:06 -0500

    Refactor ingredient selection and API request formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 
-ingredients_list = st.multiselect (
+ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe,
+    pd_df['FRUIT_NAME'],
     max_selections=5
 )
 if ingredients_list:
@@ -32,7 +32,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
